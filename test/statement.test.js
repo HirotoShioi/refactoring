9111f01d6e291108c17b439d2d3a99f734b5cbdf
Hiroto Shioi <shioihigg@gmail.com>
2023-09-01 16:36:01 +0900

    htmlへのレンダリング

--- test/statement.test.js
+++ test/statement.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, test } from '@jest/globals';
-import { invoice, statement, plays } from '../src/statement';
+import { invoice, statement, plays, htmlStatement } from '../src/statement';
 
 describe('statement', () => {
   test('should return statement for BigCo', () => {
@@ -11,5 +11,27 @@ describe('statement', () => {
     expect(lines[3]).toBe(' Othello: $500.00 (40 seats)');
     expect(lines[4]).toBe('Amount owed is $1,730.00');
     expect(lines[5]).toBe('You earned 47 credits');
+  });
+});
+
+describe('htmlStatement', () => {
+  test('should return htmlStatement for BigCo', () => {
+    const result = htmlStatement(invoice[0], plays);
+    const lines = result.split('\n');
+    expect(lines[0]).toBe('<h1>Statement for BigCo</h1>');
+    expect(lines[1]).toBe('<table>');
+    expect(lines[2]).toBe('<tr><th>play</th><th>seats</th><th>cost</th></tr>');
+    expect(lines[3]).toBe(
+      ' <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>',
+    );
+    expect(lines[4]).toBe(
+      ' <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>',
+    );
+    expect(lines[5]).toBe(
+      ' <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>',
+    );
+    expect(lines[6]).toBe('</table>');
+    expect(lines[7]).toBe('<p>Amount owed is <em>$1,730.00</em></p>');
+    expect(lines[8]).toBe('<p>You earned <em>47</em> credits</p>');
   });
 });
