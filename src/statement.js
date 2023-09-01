4a74f8c992385f0a23376d38ba92959644786639
Hiroto Shioi <shioihigg@gmail.com>
2023-09-01 16:49:01 +0900

    テスト修正

--- src/statement.js
+++ src/statement.js
@@ -48,7 +48,7 @@ export const htmlStatement = function (invoice, plays) {
 function renderHtml(data) {
   let result = `<h1>Statement for ${data.customer}</h1>\n`;
   result += '<table>\n';
-  result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>\n';
+  result += ' <tr><th>play</th><th>seats</th><th>cost</th></tr>\n';
   for (const perf of data.performances) {
     result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
     result += `<td>${usd(perf.amount)}</td></tr>\n`;
--- test/statement.test.js
+++ test/statement.test.js
@@ -4,34 +4,32 @@ import { invoice, statement, plays, htmlStatement } from '../src/statement';
 describe('statement', () => {
   test('should return statement for BigCo', () => {
     const result = statement(invoice[0], plays);
-    const lines = result.split('\n');
-    expect(lines[0]).toBe('Statement for BigCo');
-    expect(lines[1]).toBe(' Hamlet: $650.00 (55 seats)');
-    expect(lines[2]).toBe(' As You Like It: $580.00 (35 seats)');
-    expect(lines[3]).toBe(' Othello: $500.00 (40 seats)');
-    expect(lines[4]).toBe('Amount owed is $1,730.00');
-    expect(lines[5]).toBe('You earned 47 credits');
+    const expected = `
+Statement for BigCo
+ Hamlet: $650.00 (55 seats)
+ As You Like It: $580.00 (35 seats)
+ Othello: $500.00 (40 seats)
+Amount owed is $1,730.00
+You earned 47 credits
+`;
+    expect(result).toBe(expected.trimStart());
   });
 });
 
 describe('htmlStatement', () => {
   test('should return htmlStatement for BigCo', () => {
     const result = htmlStatement(invoice[0], plays);
-    const lines = result.split('\n');
-    expect(lines[0]).toBe('<h1>Statement for BigCo</h1>');
-    expect(lines[1]).toBe('<table>');
-    expect(lines[2]).toBe('<tr><th>play</th><th>seats</th><th>cost</th></tr>');
-    expect(lines[3]).toBe(
-      ' <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>',
-    );
-    expect(lines[4]).toBe(
-      ' <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>',
-    );
-    expect(lines[5]).toBe(
-      ' <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>',
-    );
-    expect(lines[6]).toBe('</table>');
-    expect(lines[7]).toBe('<p>Amount owed is <em>$1,730.00</em></p>');
-    expect(lines[8]).toBe('<p>You earned <em>47</em> credits</p>');
+    const expected = `
+<h1>Statement for BigCo</h1>
+<table>
+ <tr><th>play</th><th>seats</th><th>cost</th></tr>
+ <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>
+ <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>
+ <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>
+</table>
+<p>Amount owed is <em>$1,730.00</em></p>
+<p>You earned <em>47</em> credits</p>
+`;
+    expect(result).toBe(expected.trimStart());
   });
 });
