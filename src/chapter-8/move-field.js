ff677b55169a02ffc7cb53225689fcd61e103bbd
Hiroto Shioi <shioihigg@gmail.com>
2023-09-11 16:26:27 +0900

    フィールドの移動

---
+++ src/chapter-8/move-field.js
@@ -0,0 +1,23 @@
+export class Customer {
+  constructor(name, discountRate) {
+    this._name = name;
+    this._discountRate = discountRate;
+    this._contract = new CustomerContract(new Date());
+  }
+  get discountRate() {
+    return this._discountRate;
+  }
+  becomePreferred() {
+    this._discountRate += 0.03;
+    // other nice things
+  }
+  applyDiscount(amount) {
+    return amount.subtract(amount.multiply(this._discountRate));
+  }
+}
+
+export class CustomerContract {
+  constructor(startDate) {
+    this._startDate = startDate;
+  }
+}
