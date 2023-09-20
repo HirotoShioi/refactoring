20440491273ed40e64f8194508727f816f690060
Hiroto Shioi <shioihigg@gmail.com>
2023-09-20 13:26:43 +0900

    とり

--- src/chapter-10/replace-conditional-with-polymorphism.js
+++ src/chapter-10/replace-conditional-with-polymorphism.js
@@ -7,27 +7,61 @@ export function speeds(birds) {
 }
 
 function plumage(bird) {
-  switch (bird.type) {
-    case 'EuropeanSwallow':
-      return 'average';
-    case 'AfricanSwallow':
-      return bird.numberOfCoconuts > 2 ? 'tired' : 'average';
-    case 'NorwegianBlueParrot':
-      return bird.voltage > 100 ? 'scorched' : 'beautiful';
-    default:
-      return 'unknown';
-  }
+  return createBird(bird).plumage;
 }
 
 function airSpeedVelocity(bird) {
-  switch (bird.type) {
+  return createBird(bird).airSpeedVelocity;
+}
+
+const createBird = (birdObject) => {
+  switch (birdObject.type) {
     case 'EuropeanSwallow':
-      return 35;
+      return new EuropeanSwallow(birdObject);
     case 'AfricanSwallow':
-      return 40 - 2 * (bird.numberOfCoconuts || 0);
+      return new AfricanSwallow(birdObject);
     case 'NorwegianBlueParrot':
-      return bird.isNailed ? 0 : 10 + (bird.voltage / 10 || 0);
+      return new NorwegianBlueParrot(birdObject);
     default:
-      return null;
+      return new Bird(birdObject);
+  }
+};
+
+class Bird {
+  constructor(birdObject) {
+    Object.assign(this, birdObject);
+  }
+  get plumage() {
+    return 'unknown';
+  }
+  get airSpeedVelocity() {
+    return null;
+  }
+}
+
+class EuropeanSwallow extends Bird {
+  get plumage() {
+    return 'average';
+  }
+  get airSpeedVelocity() {
+    return 35;
+  }
+}
+
+class AfricanSwallow extends Bird {
+  get plumage() {
+    return this.numberOfCoconuts > 2 ? 'tired' : 'average';
+  }
+  get airSpeedVelocity() {
+    return 40 - 2 * (this.numberOfCoconuts || 0);
+  }
+}
+
+class NorwegianBlueParrot extends Bird {
+  get plumage() {
+    return this.voltage > 100 ? 'scorched' : 'beautiful';
+  }
+  get airSpeedVelocity() {
+    return this.isNailed ? 0 : 10 + (this.voltage / 10 || 0);
   }
 }
